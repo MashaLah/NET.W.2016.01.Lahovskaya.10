a083949e51ad4e4143ab6edb29c530a87b48de22
MashaLah <lahoska91@mail.ru>
2016-11-13 22:15:29 +0300

    Add Task1.

--- Task1/Countdown.cs
+++ Task1/Countdown.cs
@@ -2,12 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task1
 {
     public class Countdown
     {
+        private int time;
+        public Countdown(int time)
+        {
+            this.time = time;
+        }
+        public event EventHandler CountdownFinished;
 
+        protected virtual void OnCountdownFinished(Object sender, EventArgs e)
+        {
+            CountdownFinished?.Invoke(sender, e);
+        }
+
+        public void StartCountdown()
+        {
+            for (int i = time; i > 0; i--)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+            OnCountdownFinished(this, new EventArgs());
+        }
+    }
+
+    public class PositiveListener
+    {
+        public PositiveListener(Countdown countdown)
+        {
+            countdown.CountdownFinished += countdown_CountdownFinished;
+        }
+
+        public void Unregister(Countdown countdown)
+        {
+            countdown.CountdownFinished -= countdown_CountdownFinished;
+        }
+
+        private void countdown_CountdownFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hurray!");
+        }
+    }
+
+    public class NegativeListener
+    {
+        public NegativeListener(Countdown countdown)
+        {
+            countdown.CountdownFinished += countdown_CountdownFinished;
+        }
+
+        public void Unregister(Countdown countdown)
+        {
+            countdown.CountdownFinished -= countdown_CountdownFinished;
+        }
+
+        private void countdown_CountdownFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("Oh no!");
+        }
     }
 }
---
+++ Task1UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task1UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter time:");
+            int time = int.Parse(Console.ReadLine());
+            Countdown countdown = new Countdown(time);
+            PositiveListener positiveListener = new PositiveListener(countdown);
+            NegativeListener negativeListener = new NegativeListener(countdown);
+            countdown.StartCountdown();
+            Console.ReadLine();
+        }
+    }
+}
