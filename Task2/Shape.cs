3aa2149d7a0fbd13643b066dda99081cf8ae9915
MashaLah <lahoska91@mail.ru>
2016-11-13 20:16:22 +0300

    Add Task2.

---
+++ Task2/Shape.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Task2
+{
+    public interface IShape
+    {
+        double Perimeter { get; }
+        double Area { get; }
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            if (radius <= 0) throw new ArgumentException($"Parameter {nameof(radius)} must be > 0.");
+            this.radius = radius;
+        }
+        public double Perimeter
+        {
+            get {return 2 * PI * radius; }
+        }
+        public double Area
+        {
+            get { return PI * Pow(radius, 2); }
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+        public Triangle(int sideA, int sideB, int sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Every parameter must be > 0.");
+            if (sideA > sideB + sideC || sideB > sideA + sideC || sideC > sideA + sideB)
+                throw new ArgumentException("It is impossible to make a triangle with entered length of sides.");
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+        public double Perimeter
+        {
+            get { return sideA + sideB + sideC; }
+        }
+        public double Area
+        {
+            get { return Sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC)); }
+        }
+        private double halfPerimeter => Perimeter / 2;
+    }
+
+    public class Square : IShape 
+    {
+        private double side;
+        public double Side { get; set; }
+        public Square(double side)
+        {
+            if (side <= 0) throw new ArgumentException($"Parameter {nameof(side) } must be > 0.");
+            this.side = side;
+        } 
+        public double Perimeter
+        {
+            get { return side * 4; }
+        }
+        public double Area
+        {
+            get { return Pow(side, 2); }
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        private double sideA;
+        private double sideB;
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public Rectangle(double sideA, double sideB)
+        {
+            if (sideA <= 0 || sideB <= 0) throw new ArgumentException("Every parameter must be > 0.");
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+        public double Perimeter
+        {
+            get { return (sideA + sideB) * 2; }
+        }
+        public double Area
+        {
+            get { return sideA * sideB; }
+        }
+    }
+}
---
+++ Task2Tests/ShapeTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task2;
+using System.Collections;
+
+namespace Task2Tests
+{
+    [TestFixture]
+    public class ShapeTests
+    {
+        /// <summary>
+        /// A test for Circle constructor.
+        /// </summary>
+        [Test]
+        public void Constructor_InvalidData_Exception()
+        {
+            Assert.Throws<ArgumentException>(() => new Circle(-5));
+        }
+
+        /// <summary>
+        /// A test for Perimeter.
+        /// </summary>
+        [Test, TestCaseSource(nameof(ValidTestCasesPerimeter))]
+        public void Perimeter_ReternValidPerimeter(IShape obj, double expected)
+        {
+            Assert.AreEqual(obj.Perimeter, expected);
+        }
+
+        /// <summary>
+        /// A test for Area.
+        /// </summary>
+        [Test, TestCaseSource(nameof(ValidTestCasesArea))]
+        public void Area_ReternValidArea(IShape obj, double expected)
+        {
+            Assert.AreEqual(obj.Area, expected);
+        }
+
+        /// <summary>
+        /// Test cases for Perimeter/
+        /// </summary>
+        static IEnumerable ValidTestCasesPerimeter
+        {
+            get
+            {
+                yield return new TestCaseData(new Circle(3), 6 * Math.PI);
+                yield return new TestCaseData(new Rectangle(3,2),10);
+                yield return new TestCaseData(new Triangle(3,2,4),9);
+                yield return new TestCaseData(new Square(3),12);
+            }
+        }
+
+        /// <summary>
+        /// Test cases for Area.
+        /// </summary>
+        static IEnumerable ValidTestCasesArea
+        {
+            get
+            {
+                yield return new TestCaseData(new Circle(3), 9*Math.PI);
+                yield return new TestCaseData(new Rectangle(3, 2), 6);
+                yield return new TestCaseData(new Triangle(3, 3, 4),Math.Sqrt(20));
+                yield return new TestCaseData(new Square(3), 9);
+            }
+        }
+    }
+}
