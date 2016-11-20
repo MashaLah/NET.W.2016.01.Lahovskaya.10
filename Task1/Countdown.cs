9a209c8eb9429b22980bfc6c9f84a252e2c37aff
MashaLah <lahoska91@mail.ru>
2016-11-20 14:12:33 +0300

    Comments

--- Task1/Countdown.cs
+++ Task1/Countdown.cs
@@ -10,18 +10,31 @@ namespace Task1
     public class Countdown
     {
         private int time;
+
         public Countdown(int time)
         {
             this.time = time;
         }
+
+        /// <summary>
+        /// Declare the event.
+        /// </summary>
         public event EventHandler<CountdownFinishedEventArgs> CountdownFinished = delegate { };
 
+        /// <summary>
+        /// Informs listeners about event.
+        /// </summary>
+        /// <param name="e">Message to listeners.</param>
         protected virtual void OnCountdownFinished(CountdownFinishedEventArgs e)
         {
             EventHandler<CountdownFinishedEventArgs> temp = CountdownFinished;
             temp?.Invoke(this, e);
         }
 
+        /// <summary>
+        /// Put input data into event.
+        /// </summary>
+        /// <param name="message">Data that method will transfer to event</param>
         public void StartCountdown(string message)
         {
             Thread.Sleep(1000 * time);
@@ -29,6 +42,9 @@ namespace Task1
         }
     }
 
+    /// <summary>
+    /// Information that listeners will receive.
+    /// </summary>
     public sealed class CountdownFinishedEventArgs : EventArgs
     {
         public CountdownFinishedEventArgs(string message)
